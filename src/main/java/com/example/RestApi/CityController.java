b1447efe0f9367470d9bafb40e486cb0758750d1
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-05 02:02:28 +0300

    JPA and Hibernate

--- src/main/java/com/example/RestApi/CityController.java
+++ src/main/java/com/example/RestApi/CityController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.Business.ICityService;
 import com.example.entities.City;
 
+
 @RestController
 @RequestMapping("/api")
 public class CityController {
